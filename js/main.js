49019eae5581f0f12d4c109002d4c6586bc1e955
Artyom Khannanov <a.simonov90@ya.ru>
2021-01-18 12:11:00 +0300

    попытка добавить анимацию на work-with-us и некоторые новые стили в css

--- js/main.js
+++ js/main.js
@@ -73,10 +73,17 @@ $(function () {
       }, 1500);
     }
   });
-
+  
   $(window).on("scroll", function() {
-    $(".header-bottom").toggleClass("scrolled", $(this).scrollTop() > $(window).height()-750);
+    $(".header-bottom").toggleClass("scrolled", $(this).scrollTop() > $(window).height()-350);
+    $(".header-top").toggleClass("scrolled-red", $(this).scrollTop() > $(window).height()-350); 
     $(".header-top__inner").toggleClass("white", $(this).scrollTop() > $(window).height()-350);
+
+
+    // $(".header-fixed").toggleClass("scrolled", $(this).scrollTop() > $(window).height());
+
+    // $("fixed-header").toggleClass("scrolled", $(this).scrollTop() > $(window).height()-750);
+
   });
 
   $(".requisites__wrap").hide()
@@ -717,9 +724,117 @@ $(function () {
 //   document.addEventListener.addClass('vacancy__hide')
 // }
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#vacancies-hide').addEventListener('click', function() {
-    document.querySelector('.vacancies__btn-menu').classList.toggle('vacancy__hide')
-    console.log('click!')
-  })
-})
+// window.addEventListener('DOMContentLoaded', function() {
+//   document.querySelector('#vacancies-hide').addEventListener('click', function() {
+//     document.querySelector('.vacancies__btn-menu').classList.toggle('vacancy__hide')
+//     console.log('click!')
+//   })
+// })
+
+
+
+
+// Анимация на странице work-with-us
+
+let SEPARATION = 70, AMOUNTX = 50, AMOUNTY = 50;
+			let container, stats;
+			let camera, scene, renderer;
+			let particles, particle, count = 0;
+			let mouseX = 0, mouseY = 0;
+			let windowHalfX = window.innerWidth / 2;
+			let windowHalfY = window.innerHeight / 2;
+			init();
+			animate();
+			function init() {
+				container = document.createElement('div',{  id: "particles", class: "particles"});
+        
+ document.getElementById('work-animation').appendChild( container );
+				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+				camera.position.z = 1000;
+				scene = new THREE.Scene();
+				particles = new Array();
+				let PI2 = Math.PI * 2;
+				let material = new THREE.SpriteCanvasMaterial( {
+					//color: 0x3f6e86,
+         color: '#565656',
+          // color: 0xffffff,
+					program: function ( context ) {
+						context.beginPath();
+						context.arc( 0, 0, .3, 0, PI2, true );
+						context.fill();
+					}
+				} );
+				let i = 0;
+				for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+					for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+						particle = particles[ i ++ ] = new THREE.Sprite( material );
+						particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
+						particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
+           // scene.background = new THREE.Color( 0x254a5d ); // UPDATED
+						scene.add( particle );
+					}
+				}
+				renderer = new THREE.CanvasRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+        renderer = new THREE.CanvasRenderer( { alpha: true }); // gradient
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+				//stats = new Stats();
+				//container.appendChild( stats.dom );
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+				document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+				//
+				window.addEventListener( 'resize', onWindowResize, false );
+			}
+			function onWindowResize() {
+				windowHalfX = window.innerWidth / 2;
+				windowHalfY = window.innerHeight / 2;
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+			//
+			function onDocumentMouseMove( event ) {
+				mouseX = event.clientX - windowHalfX;
+				mouseY = event.clientY - windowHalfY;
+			}
+			function onDocumentTouchStart( event ) {
+				if ( event.touches.length === 1 ) {
+					event.preventDefault();
+					mouseX = event.touches[ 0 ].pageX - windowHalfX;
+					mouseY = event.touches[ 0 ].pageY - windowHalfY;
+				}
+			}
+			function onDocumentTouchMove( event ) {
+				if ( event.touches.length === 1 ) {
+					event.preventDefault();
+					mouseX = event.touches[ 0 ].pageX - windowHalfX;
+					mouseY = event.touches[ 0 ].pageY - windowHalfY;
+				}
+			}
+			//
+			function animate() {
+				requestAnimationFrame( animate );
+				render();
+				//stats.update();
+       
+			}
+			function render() {
+				camera.position.x += ( mouseX - camera.position.x ) * .05;
+				camera.position.y += ( - mouseY - camera.position.y ) * .05;
+				camera.lookAt( scene.position );
+				let i = 0;
+				for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+					for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+						particle = particles[ i++ ];
+						particle.position.y = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
+							( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+						particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 4 +
+							( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 4;
+					}
+				}
+        
+				renderer.render( scene, camera );
+				count += 0.1;
+			}
